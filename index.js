6bf6802c33ddb15f337dc717d0f55149c6f494a4
Simon Lepel <simbo@simbo.de>
2015-09-02 23:39:16 +0200

    accept tasksDir and cwd as constructor arguments

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 var Plug = require('./lib/plug.js');
 
-module.exports = function(gulp) {
-    return new Plug(gulp);
+module.exports = function(gulp, tasksDir, cwd) {
+    return new Plug(gulp, tasksDir, cwd);
 };
